3fd2c816e7c5f48580b45925bb61138cb62ffc43
MinhQuyennn <20521824@gm.uit.edu.vn>
2024-05-16 15:24:43 +0700

    update tip

--- final/backend/routes/meeting.js
+++ final/backend/routes/meeting.js
@@ -4,7 +4,7 @@ const meetingController = require('../controller/mettingController');
 
 meetingRouter.get("/getAllMeetingApprove" , meetingController.meetingApprove);
 meetingRouter.post("/createMeetingApprove" , meetingController.createmeetingApprove);
-meetingRouter.get("/getAllMeetingApprovebyID/:id" , meetingController.getAllcreatemeetingApproveBymeetingID);
+meetingRouter.get("/getAllMeetingApprovebyID/:meeting_id" , meetingController.getAllCreateMeetingApproveByMeetingID);
 
 
 module.exports = meetingRouter;
